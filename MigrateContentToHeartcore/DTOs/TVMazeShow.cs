991c068c751f75efdd7a0cd1e037a35922faaed2
Antony Meyn <anm@umbraco.dk>
2024-10-16 12:51:27 +0000

    first commit

---
+++ MigrateContentToHeartcore/DTOs/TVMazeShow.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MigrateContentToHeartcore.DTOs
+{
+	public class TVMazeShow
+	{
+		[JsonPropertyName("id")] public int Id { get; set; }
+		[JsonPropertyName("name")] public string? Name { get; set; }
+		[JsonPropertyName("image")] public Image? Image { get; set; }
+	}
+
+
+	public class Image
+	{
+		[JsonPropertyName("medium")] public string? Medium { get; set; }
+		[JsonPropertyName("original")] public string? Original { get; set; }
+	}
+}
---
+++ MigrateContentToHeartcore/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MigrateContentToHeartcore.DTOs;
+using System.Diagnostics;
+using System.Net.Http.Formatting;
+
+int page = 0;
+
+Uri ShowsAPI(int page) => new($"https://api.tvmaze.com/shows?page={page}");
+
+HttpClient client = new();
+
+Stopwatch totalTime = Stopwatch.StartNew();
+while (true)
+{
+	var response = client.GetAsync(ShowsAPI(page++)).Result;
+	var json = response.Content.ReadAsStringAsync().Result;
+	var shows = response.Content.ReadAsAsync<TVMazeShow[]>(formatters: [new JsonMediaTypeFormatter()]).Result;
+	try { response.EnsureSuccessStatusCode(); } catch { break; }
+	if (shows.Any())
+	{
+		foreach (var show in shows)
+		{
+			Console.WriteLine($"Check if {show.Name} exists in your Heartcore project using the ID: {show.Id}");
+			/*
+			 * if it exists, make sure if a specific property in Heartcore does not have a value but has a value from TVMAZE then update the property.
+			 * if it does not exist, make sure to download the images to the media section and reference it to a corresponding content node that you create.
+			 */
+		}
+	}
+}
+totalTime.Stop();
+Console.WriteLine(totalTime.Elapsed.ToString());
